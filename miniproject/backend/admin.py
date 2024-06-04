30e3a23f30ebc277de0e067033f02fcc55afedb2
zzhaann <zhhaann@mail.ru>
2024-06-04 15:26:45 +0600

    changes in admin py

--- miniproject/backend/admin.py
+++ miniproject/backend/admin.py
@@ -15,6 +15,37 @@ class UserAdmin(BaseUserAdmin):
     list_display_links = ["email"]
     list_filter = ["email", "first_name", "last_name", "is_staff", "is_active"]
     search_fields = ["email", "first_name", "last_name"]
+    fieldsets = (
+        (
+            _("Login Credentials"), {
+                "fields": ("email", "password",)
+            },
+        ),
+        (
+            _("Personal Information"),
+            {
+                "fields": ('first_name', 'last_name',)
+            },
+        ),
+        (
+            _("Permissions and Groups"),
+            {
+                "fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")
+            },
+        ),
+        (
+            _("Important Dates"),
+            {
+                "fields": ("last_login",)
+            },
+        ),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "first_name", "last_name", "password1", "password2", "is_staff", "is_active"),
+        },),
+    )
 
 
 admin.site.register(User, UserAdmin)
