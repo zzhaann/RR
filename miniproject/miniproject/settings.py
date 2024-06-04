00245200a64753098e1b156b0f318d2c0ad8f30b
zzhaann <zhhaann@mail.ru>
2024-06-04 13:19:08 +0600

    adding react

--- miniproject/miniproject/settings.py
+++ miniproject/miniproject/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+]
 
 # Application definition
 
@@ -37,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend.apps.BackendConfig',
-
+    'backend',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
